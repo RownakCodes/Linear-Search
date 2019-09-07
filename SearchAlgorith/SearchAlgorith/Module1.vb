1efaab029ddc5900b599bc9a8366e01c3b321046
RownakCodes <47391823+RownakCodes@users.noreply.github.com>
2019-09-07 17:03:48 +0600

    Update Module1.vb

--- SearchAlgorith/SearchAlgorith/Module1.vb
+++ SearchAlgorith/SearchAlgorith/Module1.vb
@@ -2,34 +2,6 @@
 
     Sub Main()
 
-        'Dim mid, start, finish, key As Integer
-        'Dim found As Boolean = False
-
-        'start = 0
-        'finish = UBound(arr)
-
-        'Console.WriteLine("What do you want to search? ")
-        'key = Console.ReadLine()
-
-        'While (start < finish)
-
-        '    mid = CInt((start + finish) / 2)
-
-        '    If arr(mid) = key Then
-        '        found = True
-        '        Exit While
-        '    ElseIf arr(mid) < key Then
-        '        start = mid + 1
-        '    Else
-        '        finish = mid - 1
-        '    End If
-        'End While
-
-        'If found = True Then
-        '    Console.WriteLine("The thing is in the array and is at {0} no. index", mid)
-        'Else
-        '    Console.WriteLine("Ain't in the array")
-        'End If
 
         Dim size, find, pos As Integer
 
@@ -43,11 +15,14 @@
             arr(x) = Console.ReadLine()
         Next
 
+    
+    
         Console.Clear()
         Console.WriteLine("which value do you want to ensure if you entered?")
         find = Console.ReadLine()
-
+    
         Dim found As Boolean
+    
         For x = 0 To UBound(arr)
             If arr(x) = find Then
                 found = True
@@ -56,8 +31,11 @@
             End If
         Next
 
+
         If found = True Then
             Console.WriteLine("you entered the number {0}, and it is in index: {1}", find, pos)
+        Else
+            Console.WriteLine("It ain't in the list you entered!")
         End If
 
     End Sub
